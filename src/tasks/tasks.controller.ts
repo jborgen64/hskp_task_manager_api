e6976c1a9683b73f279a410d82f43e2286c7bfb4
son loku <jacobrborgen@gmail.com>
2020-09-29 23:35:37 -0500

    added DTO file for post route function

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
     @Get()
     findAll(): string {
         return 'Get all them tasks';
+    }
+
+    @Post()
+    create(): string {
+        return 'Post all them tasks';
     }
 }
