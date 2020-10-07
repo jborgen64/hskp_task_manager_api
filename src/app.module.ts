7e4dbe09370141291189f5c3d10b31078da9aa4d
son loku <jacobrborgen@gmail.com>
2020-10-07 11:34:40 -0500

    added an interceptor to help track performance of API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
-import { APP_FILTER } from '@nestjs/core';
-import { HttpErrorFilter } from './shared/http_error.filter'
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpErrorFilter } from './shared/http_error.filter';
+import { LoggingInterceptor } from './shared/logging.interceptor';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), TasksModule],
   controllers: [AppController],
-  providers: [AppService, {
-  provide: APP_FILTER,
-  useClass: HttpErrorFilter
-  }]
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    }
+  ],
 })
 export class AppModule {}
